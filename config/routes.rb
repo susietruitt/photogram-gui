5d444def38ee16aea1ccc28b555464336b1f2f98
susietruitt <91689219+susietruitt@users.noreply.github.com>
2024-05-03 02:57:29 +0000

    photos index

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ get("/users", { :controller => "users", :action => "index" })
 
 get("/users/:path_username", { :controller => "users", :action => "show" })
 
-get
+get("/photos", { :controller => "photos", :action => "index" })
 
 
 end
