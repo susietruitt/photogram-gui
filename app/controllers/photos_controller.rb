08ec8baca5bfac8fb32a04167bb060f07406839a
susietruitt <91689219+susietruitt@users.noreply.github.com>
2024-05-03 05:00:53 +0000

    committing for the night

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -7,4 +7,29 @@ class PhotosController < ApplicationController
     render ({:template => "photo_templates/index"})
   end
 
+
+  def show
+    photo_username = params.fetch("path_id")
+  
+    matching_photos = Photo.where({ :id => photo_username})
+  
+    @the_photo = matching_photos.at(0)
+  
+    render ({ :template => "photo_templates/show"})
+  
+  end
+
+  def destroy
+    the_id = params.fetch("path_id")
+
+    matching_photos = Photo.where({ :id=> the_id})
+
+    the_photo= matching_photos.at(0)
+
+    the_photo.destroy
+
+    Also 
+
+    render({ :template=> "photo_templates/destroy"})
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,8 @@ get("/users/:path_username", { :controller => "users", :action => "show" })
 
 get("/photos", { :controller => "photos", :action => "index" })
 
+get("/photos/:path_id", { :controller => "photos", :action => "show" })
+
+get("/delete_photo/:path_id", { :controller => "photos", :action => "destroy" })
 
 end
