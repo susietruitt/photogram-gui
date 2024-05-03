d123dc479e9d0da720f3f4e63540442503100d93
susietruitt <91689219+susietruitt@users.noreply.github.com>
2024-05-03 01:22:13 +0000

    homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
 
+get("/users", { :controller => "users", :action => "index" })
+
+get("/users/:username", { :controller => "users", :action => "show" })
+
+
+
 end
